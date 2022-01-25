a8be521c777ccc728dc97abe28afa0ee8790babd
Md Samsuzzoha <mdshayon0@gmail.com>
2022-01-25 07:32:48 +0600

    change table name

--- server/controllers/subscriber.controller.js
+++ server/controllers/subscriber.controller.js
@@ -1,5 +1,5 @@
 const db = require('../models');
-const { Subscriber } = db;
+const { subscribers } = db;
 
 
 
@@ -9,14 +9,14 @@ const addSubscriber = async (req, res) => {
     const { email } = req.body;
     if (!email) return res.status(406).json({ msg: "Please provide email address" });
 
-    const emailExist = await Subscriber.findOne({ where: { email } });
+    const emailExist = await subscribers.findOne({ where: { email } });
     if (emailExist) {
         res.status(208).json({ msg: "already registered", emailExist });
     } else {
-        const subscriber = await Subscriber.create({
+        const subscriber = await subscribers.create({
             email: req.body.email
         });
-        res.status(200).json({ msg: "Created new subscriber", subscriber });
+        res.status(200).json({ msg: "Created new subscribers", subscriber });
     }
 }
 
@@ -26,8 +26,8 @@ const addSubscriber = async (req, res) => {
 const getAllSubscribers = async (req, res) => {
 
     try {
-        const subscribers = await Subscriber.findAll({});
-        res.status(400).json({ msg: "Get all subscribers", subscribers });
+        const subscriber = await subscribers.findAll({});
+        res.status(400).json({ msg: "Get all subscriberss", subscriber });
     } catch (error) {
         throw error;
     }
