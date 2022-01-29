b66a61ed2ad99314ca7276c71775889632685ce8
Md Samsuzzoha <mdshayon0@gmail.com>
2022-01-29 06:51:13 +0600

    client production build

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,5 +1,5 @@
-const BACKEND_URL = "http://localhost:4000/api";
-// const BACKEND_URL = "http://18.118.158.200/api";
+// const BACKEND_URL = "http://localhost:4000/api";
+const BACKEND_URL = "http://18.118.158.200/api";
 
 export {
     BACKEND_URL
