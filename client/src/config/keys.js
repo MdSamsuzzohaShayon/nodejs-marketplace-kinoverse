f0f3a3d862ebe0a94c57ee89d346f38be3770cde
Md Samsuzzoha <mdshayon0@gmail.com>
2022-02-06 04:36:28 +0600

    url fixing

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,5 +1,6 @@
 // const BACKEND_URL = "http://localhost:4000/api";
-const BACKEND_URL = "http://18.118.158.200/api";
+// const BACKEND_URL = "http://18.118.158.200/api";
+const BACKEND_URL = "https://kinoverse.net/api";
 
 export {
     BACKEND_URL
