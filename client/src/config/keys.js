4e1f6d9f1da5b70ce381644216c6e18146fd70c7
Md Samsuzzoha <mdshayon0@gmail.com>
2022-04-08 09:33:47 +0600

    google tag for set added

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -2,7 +2,7 @@
 
 
 // const BACKEND_URL = "http://localhost:4000/api";
-const BACKEND_URL = "http://161.35.5.184/api"; // Digital Ocean
+const BACKEND_URL = "http://67.205.140.215/api"; // Digital Ocean
 // const BACKEND_URL = "https://kinoverse.net/api";
 // lets make a comment 
 
